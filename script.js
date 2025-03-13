d9532dc1f0f2ed3294faa2019fe859d7bfcd7a79
mathisrueegger <mathis.rueegger@stud.krw.ch>
2025-03-13 21:51:09 +0100

    Update script.js
    Gleiches Szenario wie bei HTML Commit soeben.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.querySelectorAll('.nav-link').forEach(link => {
-   link.addEventListener('click', (e) => {
-       e.preventDefault();
-       const targetId = link.getAttribute('href').substring(1);
-       const
+document.querySelector('.burger-menu').addEventListener('click', () => {
+   document.querySelector('.nav-links').classList.toggle('active');
+});
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+   anchor.addEventListener('click', function (event) {
+       event.preventDefault();
+       document.querySelector(this.getAttribute('href')).scrollIntoView({
+           behavior: 'smooth'
+       });
+   });
+});
