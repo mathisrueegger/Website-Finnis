dc4c28720a13a8de5957f09374386c4146b68a7e
mathisrueegger <mathis.rueegger@stud.krw.ch>
2025-03-14 18:00:37 +0100

    Update script.js
    Falscher Code:)

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.querySelector('.burger-menu').addEventListener('click', () => {
    document.querySelector('.nav-links').classList.toggle('active');
 });
 
-document.querySelectorAll('nav ul li a').forEach(anchor => 
-   anchor.addEventListener('click', function (event) {
-      if (this.getAttribute('href').startsWith('#')) {
-         event.preventDefault();
-         document.querySelector(this.getAttribute('href')).scrollIntoView({
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (event) {
+        if (this.getAttribute('href').startsWith('#')) {
+            event.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
-         });
-      }
-   });
+            });
+        }
+    });
 });
