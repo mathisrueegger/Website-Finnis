27c95c4b584adc4f632d6326892a7bfcce012fe8
mathisrueegger <mathis.rueegger@stud.krw.ch>
2025-03-14 17:56:56 +0100

    Update script.js
    Anpassung des Smooth Scrolling.

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ document.querySelector('.burger-menu').addEventListener('click', () => {
    document.querySelector('.nav-links').classList.toggle('active');
 });
 
-document.querySelectorAll('nav ul li a').forEach(anchor => {
+document.querySelectorAll('nav ul li a').forEach(anchor => 
    anchor.addEventListener('click', function (event) {
-       event.preventDefault();
-       document.querySelector(this.getAttribute('href')).scrollIntoView({
-           behavior: 'smooth'
-       });
+      if (this.getAttribute('href').startsWith('#')) {
+         event.preventDefault();
+         document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+         });
+      }
    });
 });
