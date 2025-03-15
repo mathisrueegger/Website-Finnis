8bbd7ebd28894e7499fc743ac608c9463fe4ef8e
FinnBertschinger <finn.bertschinger@stud.krw.ch>
2025-03-15 15:56:10 +0100

    Update db_connect.php
    Verbindung zur MySQL-Datenbank

--- db_connect.php
+++ db_connect.php
@@ -1,1 +1,12 @@
-Verbindung zur Datenbank
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "kontaktformularfinnis_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+?>
