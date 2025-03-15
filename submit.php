588d6803b37af53edafb682aacf9103236d26440
FinnBertschinger <finn.bertschinger@stud.krw.ch>
2025-03-15 15:58:17 +0100

    Update submit.php
    Formularabsendung und Datenbankeintrag

--- submit.php
+++ submit.php
@@ -1,3 +1,21 @@
 <?php
-echo "PHP funktioniert!";
+
+include('db_connect.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $nachricht = $_POST['message'];
+
+    $sql = "INSERT INTO einsendungen (name, email, message, date) VALUES ('$name', '$email', '$nachricht', NOW())";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: danke.html");
+        exit();
+    } else {
+        echo "Fehler: " . $conn->error;
+    }
+
+    $conn->close();
+}
 ?>
